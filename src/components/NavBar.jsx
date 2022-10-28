af8fa98e9a866342d7349f2845ea3814568b2ab3
Vicky Lee <ytleevicky@gmail.com>
2022-10-28 12:48:02 -0400

    Fixed navbar position

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Vicky</h1>
       </div>
