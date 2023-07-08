601c2d625a16ba5e7e23aa8e424213b4b1148e9c
Vicky Lee <ytleevicky@gmail.com>
2023-07-08 14:46:55 -0400

    Update profile image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
-import HeroImage2 from "../assets/heroImage3.jpg";
+import HeroImage2 from '../assets/heroImage.jpg';
 
 const About = () => {
   return (
     <div
-      name="about me"
-      className="bg-navy md:h-screen"
+      name='about me'
+      className='bg-navy md:h-screen'
     >
       <div
-        className="max-w-screen-lg mx-auto p-6 md:p-12 flex flex-col
-        justify-center w-full h-full text-lightest-slate"
+        className='max-w-screen-lg mx-auto p-6 md:p-12 flex flex-col
+        justify-center w-full h-full text-lightest-slate'
       >
-        <div className="pt-24 md:pt-0">
-          <p className="text-4xl font-bold inline border-b-4 border-lightest-slate ">
-            <span className="text-cyan">01.</span> About me
+        <div className='pt-24 md:pt-0'>
+          <p className='text-4xl font-bold inline border-b-4 border-lightest-slate '>
+            <span className='text-cyan'>01.</span> About me
           </p>
         </div>
 
-        <div className="flex flex-col md:flex-row py-6 justify-between md:space-x-14">
-          <div className="max-w-xl mb-7 md:w-4/6">
+        <div className='flex flex-col md:flex-row py-6 justify-between md:space-x-14'>
+          <div className='max-w-xl mb-7 md:w-4/6'>
             <h2>
               Hello! I'm Vicky Lee, a Junior Software Engineer currently based
               in Toronto. I'm passionate about building things for the web and
@@ -49,11 +49,11 @@ const About = () => {
           </div>
 
           {/* Image */}
-          <div className="md:w-2/6">
+          <div className='md:w-2/6'>
             <img
               src={HeroImage2}
-              alt="my profile"
-              className="rounded-3xl mx-auto w-3/4 md:w-full transform transition-all hover:scale-105"
+              alt='my profile'
+              className='rounded-3xl mx-auto w-3/4 md:w-full transform transition-all hover:scale-105'
             />
           </div>
         </div>
